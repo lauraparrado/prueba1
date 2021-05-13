38c641d94cdc16a1a7933a2e6eee52b847efc484
Altix <laespaviza@gmail.com>
2021-05-13 16:54:09 -0500

    cargado

---
+++ mysite/calculadora/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
+
+
+class CalculadoraIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
